4e5acec1037dd5365a9ac11f33129563871a48c1
Eric Hodgins <hodgins.e@gmail.com>
2016-10-24 23:42:37 -0400

    Initial Commit

---
+++ WatchConnectivityDemoTests/WatchConnectivityDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  WatchConnectivityDemoTests.swift
+//  WatchConnectivityDemoTests
+//
+//  Created by Eric Hodgins on 2016-10-24.
+//  Copyright © 2016 Eric Hodgins. All rights reserved.
+//
+
+import XCTest
+@testable import WatchConnectivityDemo
+
+class WatchConnectivityDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
